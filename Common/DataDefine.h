a778d9708cec071b47d68d3bcc975b310780533e
lp <13059973+lplp112233@user.noreply.gitee.com>
2025-07-21 17:55:49 +0800

    Update DataDefine.h

--- Common/DataDefine.h
+++ Common/DataDefine.h
@@ -6,6 +6,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <libavutil/log.h>
+#include <QDebug>
 
 // ------------------------- YUV渲染所需数据 -------------------------
 #pragma pack(push, 1)
